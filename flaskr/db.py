82ad4135937722dd5041757516e9a8e9b9b31786
Alejo Rios <alejoriosiglesias@gmail.com>
2022-07-05 11:47:21 +0100

    feat: register with the app

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,7 +1,6 @@
-from pydoc import cli
 import sqlite3
-
 import click
+
 from flask import current_app, g
 from flask.cli import with_appcontext
 
@@ -33,4 +32,10 @@ def init_db_command():
     """Clear the existing data and create new tables."""
     init_db()
     click.echo('Initialized the database.')
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
+
 
